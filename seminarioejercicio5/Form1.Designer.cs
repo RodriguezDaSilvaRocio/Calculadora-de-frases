dc044f80b65d747f47038509da66eeab2eb676ab
RocioRodriguezDaSilva <songhaseul@hotmail.com>
2024-08-06 01:50:42 -0300

    Agregar archivos de proyecto.

---
+++ seminarioejercicio5/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace seminarioejercicio5
+{
+    partial class formulario1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.frasesbox = new System.Windows.Forms.ListBox();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(363, 50);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(316, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbox_1_KeyPress);
+            // 
+            // frasesbox
+            // 
+            this.frasesbox.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.frasesbox.FormattingEnabled = true;
+            this.frasesbox.ItemHeight = 15;
+            this.frasesbox.Location = new System.Drawing.Point(12, 50);
+            this.frasesbox.Name = "frasesbox";
+            this.frasesbox.Size = new System.Drawing.Size(322, 199);
+            this.frasesbox.TabIndex = 1;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.ForeColor = System.Drawing.Color.White;
+            this.lblresultado.Location = new System.Drawing.Point(359, 85);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(167, 22);
+            this.lblresultado.TabIndex = 2;
+            this.lblresultado.Text = "Los resultados son:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(12, 275);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(322, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Limpiar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // formulario1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(714, 404);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.frasesbox);
+            this.Controls.Add(this.txtInput);
+            this.Name = "formulario1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ListBox frasesbox;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ seminarioejercicio5/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace seminarioejercicio5
+{
+    public partial class formulario1 : Form
+    {
+        public formulario1()
+        {
+            //verifico que los eventos esten conectados
+
+            InitializeComponent();
+            this.txtInput.KeyPress += new KeyPressEventHandler(this.txtbox_1_KeyPress);
+            this.frasesbox.SelectedIndexChanged += new EventHandler(this.frasesbox_SelectedIndexChanged);
+            this.btnClear.Click += new EventHandler(this.btnClear_Click);
+        }
+
+
+        private void txtbox_1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // controlar accion para tecla enter
+
+            if ((e.KeyChar) == 13)
+            {
+                //obtener frase sin espacios
+                string phrase = txtInput.Text.Trim();
+
+                //pregunto al usuario si realmente deseo agregar la palabra
+
+                DialogResult Resultado = MessageBox.Show("¿Desea agregar la palabra?", "Advertencia", MessageBoxButtons.OKCancel);
+                
+                if (Resultado == DialogResult.OK)
+                {
+                    //agrego y limpio el box
+                    frasesbox.Items.Add(phrase);
+                    txtInput.Clear();
+                    txtInput.Focus();
+
+                }
+
+                else 
+                {
+                    txtInput.Focus();
+                }
+                
+                e.Handled = false;
+            }
+
+        }
+        private void frasesbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //con el null verificio si hay un elemento seleccionado
+                if(frasesbox.SelectedItem != null)
+                {
+
+                    //llamo a los metodos
+                    string selectedPhrase = frasesbox.SelectedItem.ToString();
+
+                    int characterCount = CalculatecharacterCount(selectedPhrase);
+                    int spaceCount = CalculatespaceCount(selectedPhrase);
+                    string firstWord = GetfirstWord(selectedPhrase);
+                    string lastWord = GetlastWord(selectedPhrase);
+
+                    //muestro resultado en el label
+
+                    lblresultado.Text = "La cantidad de caracteres es: " + characterCount.ToString() + "\n" +
+                        "Cantidad de espacios vacios: " + spaceCount.ToString() + "\n" + "Primera palabra: " +
+                        firstWord + "\n" + "Ultima palabra: " + lastWord;
+                }
+            }
+            private void btnClear_Click(object sender, EventArgs e)
+            {
+            //pregunto si deseo limpiar el formulario
+                DialogResult resultado = MessageBox.Show("¿Desea limpiar todo el contenido?", "Confirmar", MessageBoxButtons.OKCancel);
+
+                if (resultado == DialogResult.OK)
+                {
+                    //limpio
+                    txtInput.Clear();
+                    frasesbox.Items.Clear();
+                    lblresultado.Text = string.Empty;
+                }
+            }
+
+        private int CalculatecharacterCount(string text)
+        {
+            //regreso longitud
+            return text.Length;
+        }
+
+        private int CalculatespaceCount(string text)
+        {
+            //cuento espacios en blanco
+            return text.Count(c => char.IsWhiteSpace(c));
+        }
+        private string GetfirstWord(string text)
+        {
+            //usando split obtengo la primera palabra
+            var words = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length > 0 ? words[0] : string.Empty;
+        }
+
+        private string GetlastWord(string text)
+        {
+            //usando split obtengo la ultima palabra
+            var words = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length > 0 ? words[words.Length - 1] : string.Empty;
+        
+        }
+
+    
+    }
+}
